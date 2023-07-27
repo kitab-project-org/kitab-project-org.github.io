23d840c76c35b2c4a01ece53ae222e83d3736ef0
mabarber92 <mathew.barber.dh@gmail.com>
2023-07-27 08:56:35 +0100

    started to update conversion script, removed test files

--- conversion_script/docx_converter.py
+++ conversion_script/docx_converter.py
@@ -14,42 +14,32 @@ from datetime import date
 from datetime import datetime
 import json
 
-# Load pandoc if needed
-if hasattr(pypandoc, 'ensure_pandoc_installed'):
-    # pylint: disable=E1101
-    pypandoc.ensure_pandoc_installed(delete_installer=True)
-else:
-    try:
-        pypandoc.get_pandoc_path()
-    except OSError:
-        pypandoc.download_pandoc()
 
+def check_pypandoc():
+  """We need this for running on github actions - if a pandoc install is not found on the os there - it will install it"""
+  # Load pandoc if needed
+  if hasattr(pypandoc, 'ensure_pandoc_installed'):
+      # pylint: disable=E1101
+      pypandoc.ensure_pandoc_installed(delete_installer=True)
+  else:
+      try:
+          pypandoc.get_pandoc_path()
+      except OSError:
+          pypandoc.download_pandoc()
 
-# Getting main directory as script path
+def set_directories()
+  """Set the directories for locating the files to convert and where to put those files when converted"""
+  abspath = os.path.abspath(sys.argv[0])
+  dname = os.path.dirname(abspath)
+  os.chdir(dname)
 
-abspath = os.path.abspath(sys.argv[0])
-dname = os.path.dirname(abspath)
-os.chdir(dname)
-
-parent = "\\".join(dname.split("\\")[:-1])
-print(parent)
-print(dname)
-
-# Setting directories
-docx_in = dname + "/input/blogs"
-header_in = dname + "/input/headers"
-image_out = "../images/blogs/" + str(date.today()) + "/"
-blog_dir = "../_posts/"
-glossary_path = dname + "/resources/glossary.json"
-
-header_plain = dname + "/resources/header_plain"
-
-docx = re.compile(".*\.docx")
-
-# Get data from glossary json
-with open(glossary_path) as f:
-  gloss = json.load(f)
-  f.close()
+  in_dir = dname + "/input"
+  header_template = dname + "/resources/header_plain"
+  archive_dir = dname + "/archive"
+  image_out = "../images/blogs/" + str(date.today()) + "/"
+  blog_dir = "../_posts/"
+  glossary_path = dname + "/resources/glossary.json"
+  return in_dir, header_template, archive_dir image_out, blog_dir, glossary_path
 
 # Check terms against a glossary and create a relevant piece of heading
 def find_terms(glossary, blog):
@@ -68,42 +58,116 @@ def find_terms(glossary, blog):
 
 
 # Create string from docx formatted text except those in wrong format
-def docx_conv(root, name, images_path):
-        author = re.findall(r"(^.*?)\.", name)[0]
-        extract_img = "--extract-media=" + images_path + author
-        path = os.path.abspath(os.path.join(root, name))
-        blog = pypandoc.convert_file(path, 'markdown-simple_tables-multiline_tables-grid_tables', extra_args =["--wrap=none", extract_img])
-        return blog, author.lower()
+def docx_conv(root, name, images_path):        
+    extract_img = "--extract-media=" + images_path + author
+    path = os.path.abspath(os.path.join(root, name))
+    blog = pypandoc.convert_file(path, 'markdown-simple_tables-multiline_tables-grid_tables', extra_args =["--wrap=none", extract_img])
+    return blog
 
 
 # Add a correctly labelled header to the blog
-def header_build (header, name, blog, glossary):
-     with open(header, encoding = "utf-8") as f:
-            head_in = f.read()
-            f.close()
-     author = re.findall(r"(^.*?)\.", name)[0]
-     author = re.sub(r"\d", "", author)
-     author_code = "author: " + author.lower() + "\n"
-     head_in = re.sub(r"author:.*\r?\n", author_code, head_in)
-     # Sub in the submitted glossary - set up so it will just replace with metadata end placer if glossary empty
-     head_in = re.sub(r"\n---", glossary, head_in)
-     images = re.findall(r"\!\[.*?\]\(.*(/images/[^)]*)\)", blog)
-     if len(images) >= 1:
-         thumb = "\nimage: \"" + images[0] + "\"\n---\n"
-         head_in = re.sub(r"\n---", thumb, head_in)
-     # else:
-     #     thumb = "\nimage: \"/images/kitab/mesa.jpg\"\n---\n"
-     
-     
-     final = head_in + "\n\n" + blog
-     title = re.findall("title:.*\"(.*)\"", head_in)
+def header_build (head_in, blog, glossary):
+    # Sub in the submitted glossary -
+    head_in = re.sub(r"\n---", glossary, head_in)
+    images = re.findall(r"\!\[.*?\]\(.*(/images/[^)]*)\)", blog)
+    if len(images) >= 1:
+        thumb = "\nimage: \"" + images[0] + "\"\n---\n"
+        head_in = re.sub(r"\n---", thumb, head_in)
+    # else:
+    #     thumb = "\nimage: \"/images/kitab/mesa.jpg\"\n---\n"
+    
+    
+    final = head_in + "\n\n" + blog
+    title = re.findall("title:.*\"(.*)\"", head_in)
 
-     if len(title) > 0 and title[0] != "":
-          title_s = re.sub(r"[\s:/.,]", "-", title[0])[:40]
-     else:
-          title_s = str(datetime.now().microsecond)[:3]
+    if len(title) > 0 and title[0] != "":
+        title_s = re.sub(r"[\s:/.,]", "-", title[0])[:40]
+    else:
+        title_s = str(datetime.now().microsecond)[:3]
 
-     return (final, title_s)
+    return (final, title_s)
+
+def fetch_glossary(glossary_path):
+  # Get data from glossary json
+  with open(glossary_path) as f:
+    gloss = json.load(f)
+  
+  return gloss
+
+def find_yml_docx(file_list):
+  """Loop through a list of directories and find the yml files return a list of dictionaries pairing the yml header to the docx files"""
+  out_list = []
+  yml = re.compile(".*\.yml")
+  for in_file in file_list:    
+    if yml.match(in_file):
+      with open(in_file, encoding='utf-8-sig') as f:
+        yml_text = f.open()  
+      corresponding_docx = re.findall(r"docx:\s*['\"](.*)['\"]\n", yml_text)[0]
+      yml_header = "".join(re.split("---", yml_text)[:3])
+      out_list.append({"docx": corresponding_docx, "header": yml_header})
+  return out_list
+
+
+def main():
+
+  # Check pypandoc install
+  check_pypandoc()
+
+  # Get all the main directories that will be used by the script
+  in_dir, header_template, archive_dir, image_out, blog_dir, glossary_path = set_directories()
+
+  # Get a list of directories in the input 
+  files_in = os.listdir(in_dir)
+
+  # Convert that list of files into a list of yml_header docx pairs
+  docx_pairs = find_yml_docx(files_in)
+
+  # Fetch the glossary for use in the conversion process
+  gloss = fetch_gloss(glossary_path)
+
+  docx_check = re.compile(".*\.docx")
+  # Loop through the pairs run the conversion and build the output text
+  for docx in docx_pairs:
+    docx_path = docx["docx"]
+    # NEED TO ADD A CHECK THAT FILE EXISTS - AND REPORT AN ERROR
+    # Check we're dealing with a docx file - otherwise we'll trip up the converter
+    if docx_checl.match(docx_path):
+        print(docx_path + " ...format correct")
+        blog_text = docx_conv(in_dir, docx_path)
+
+        # Take cleaning steps - build into function and use it here
+
+        # Use function to paste together the blog text and the header
+        final_text = header_build(docx["header"], blog_text, glossary)
+
+        # Write out the final converted text
+
+
+
+
+
+# Getting main directory as script path
+
+
+
+parent = "\\".join(dname.split("\\")[:-1])
+print(parent)
+print(dname)
+
+# Setting directories
+docx_in = dname + "/input/blogs"
+header_in = dname + "/input/headers"
+image_out = "../images/blogs/" + str(date.today()) + "/"
+blog_dir = "../_posts/"
+glossary_path = dname + "/resources/glossary.json"
+
+header_plain = dname + "/resources/header_plain"
+
+docx = re.compile(".*\.docx")
+
+
+
+
 
 
 
